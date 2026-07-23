acea357ad4117c957f0eff32b78607c53f481f8e
Milton082 <chuquimilton53@gmail.com>
2026-07-23 09:36:32 -0500

    R3

--- src/main/java/ec/edu/ups/icc/labevaluation/laboratories/repositories/LaboratoryRepository.java
+++ src/main/java/ec/edu/ups/icc/labevaluation/laboratories/repositories/LaboratoryRepository.java
@@ -1,7 +1,14 @@
 package ec.edu.ups.icc.labevaluation.laboratories.repositories;
+
 import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import ec.edu.ups.icc.labevaluation.laboratories.entities.LaboratoryEntity;
+
 public interface LaboratoryRepository extends JpaRepository<LaboratoryEntity, Long> {
-    List<LaboratoryEntity> findByActiveTrueAndDeletedFalseOrderByIdAsc();
+
+    List<LaboratoryEntity> findByCampus_IdAndCapacityGreaterThanEqualAndActiveTrueAndDeletedFalseAndCampus_ActiveTrueAndCampus_DeletedFalseOrderByCapacityDesc(
+            Long campusId,
+            Integer minCapacity);
 }
